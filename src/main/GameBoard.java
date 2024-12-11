b95eece574d379ba52f4c1a7c8f7e3675e5635ef
TheRevGodish <thomas.sajus@hotmail.fr>
2024-12-11 13:56:43 +0100

    Chess Game Project

--- src/main/GameBoard.java
+++ src/main/GameBoard.java
@@ -8,14 +8,24 @@ public class GameBoard {
     public void draw (Graphics2D gb) {
         for (int row = 0; row < 8; row++) {
             for (int col = 0; col < 8; col++) {
-                if ((col % 2) > 0){
-                    gb.setColor(new Color(130, 94, 92));
+                if ((row % 2) > 0) {
+                    if ((col % 2) > 0) {
+                        gb.setColor(new Color(130, 94, 92));
+                    } else {
+                        gb.setColor(new Color(227, 186, 143));
+                    }
+                    gb.fillRect(col * 100, row * 100, 100, 100);
                 }
                 else {
-                    gb.setColor(new Color(227, 186, 143));
+                    if ((col % 2) > 0) {
+                        gb.setColor(new Color(227, 186, 143));
+                    } else {
+                        gb.setColor(new Color(130, 94, 92));
+                    }
+                    gb.fillRect(col * 100, row * 100, 100, 100);
                 }
-                gb.fillRect(col * 100, row * 100, 100, 100);
             }
+
         }
     }
 }
