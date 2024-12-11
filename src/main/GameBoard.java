8e27e0d0486a6f27e02bcb14c80e12af8d213c69
TheRevGodish <thomas.sajus@hotmail.fr>
2024-12-11 13:42:29 +0100

    Chess Game Project

--- src/main/GameBoard.java
+++ src/main/GameBoard.java
@@ -1,12 +1,21 @@
 package main;
 
 import java.awt.*;
+import java.awt.Graphics2D;
 
 public class GameBoard {
 
     public void draw (Graphics2D gb) {
-        for (int row = 0; row < 8; row++) {// working...
-
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                if ((col % 2) > 0){
+                    gb.setColor(new Color(130, 94, 92));
+                }
+                else {
+                    gb.setColor(new Color(227, 186, 143));
+                }
+                gb.fillRect(col * 100, row * 100, 100, 100);
+            }
         }
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -6,17 +6,18 @@ public class Main {
     public static void main(String[] args) {
 
         JFrame window = new JFrame();
-        window.setTitle("Chesss Game");
-        window.setResizable(true);
+        window.setTitle("Chess Game");
+        window.setResizable(false);
         window.setVisible(true);
         window.setLocationRelativeTo(null);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+
         GamePanel gp = new GamePanel();
         window.add(gp);
         window.pack();
+        gp.launchGame();
 
-        // Window icon
         ImageIcon icon = new ImageIcon("F:/THOMAS/Projet Code/Chess Game/assets/chess icon.png");
         window.setIconImage(icon.getImage());
 
