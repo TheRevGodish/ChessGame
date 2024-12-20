8eabf4a7135c1005fcc46851ebf485975d6c583a
TheRevGodish <thomas.sajus@hotmail.fr>
2024-12-20 11:31:43 +0100

    Chess Game Project

--- src/main/Move.java
+++ src/main/Move.java
@@ -1,6 +1,8 @@
 package main;
 
 
+import static java.lang.Math.abs;
+
 public class Move extends Plateau {
 
     public boolean isValid(int x1, int y1, int x2, int y2, String type, String color) {
@@ -20,8 +22,23 @@ public class Move extends Plateau {
                 }
                 break;
 
+            case "knight":
+                break;
+
             case "bishop":
-                valid = true;
+                valid = abs(x2 - x1) == abs(y2 - y1);
+                break;
+
+            case "rook":
+                valid = (x2 == x1) || (y2 == y1);
+                break;
+
+            case "king":
+                valid = ((abs(x2 - x1) == 1) && abs(y2 - y1) < 2 || (abs(y2 - y1) == 1)) && abs(x2 - x1) < 2;
+                break;
+
+            case "queen":
+                valid = (x2 == x1) || (y2 == y1) || abs(x2 - x1) == abs(y2 - y1);
                 break;
         }
         return valid;
